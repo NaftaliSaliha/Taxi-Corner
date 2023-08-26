0a63bc39065ca6b1b798507ddff1dc96e55c1d54
Naftali Riza Lucieta Saliha <136670760+NaftaliSaliha@users.noreply.github.com>
2023-08-26 12:56:08 +0700

    Update tubes.sql

--- tubes.sql
+++ tubes.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 14, 2023 at 08:49 AM
+-- Generation Time: Aug 26, 2023 at 07:55 AM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 7.4.27
 
@@ -41,75 +41,8 @@ CREATE TABLE `driver` (
 --
 
 INSERT INTO `driver` (`id`, `nama`, `umur`, `gender`, `no_telpon`, `alamat`) VALUES
-(1, 'Exam', 12, 'Perempuan', '123', 'asd'),
-(2, 'West', 33, 'Laki-Laki', '12321', 'asd');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `login`
---
-
-CREATE TABLE `login` (
-  `id` int(11) NOT NULL,
-  `nama` varchar(255) NOT NULL,
-  `role` varchar(50) NOT NULL,
-  `username` varchar(50) NOT NULL,
-  `password` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `login`
---
-
-INSERT INTO `login` (`id`, `nama`, `role`, `username`, `password`) VALUES
-(1, 'Admin', 'Admin', 'Admin', 'Admin'),
-(2, 'Owner', 'Owner', 'Owner', 'Owner'),
-(4, 'Example', 'Admin', 'Example', 'Example');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `penggunaan`
---
-
-CREATE TABLE `penggunaan` (
-  `id` int(11) NOT NULL,
-  `id_driver` int(11) NOT NULL,
-  `id_taksi` int(11) NOT NULL,
-  `penumpang` varchar(255) NOT NULL,
-  `tanggal` varchar(50) NOT NULL,
-  `layanan` varchar(255) NOT NULL,
-  `tarif` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `penggunaan`
---
-
-INSERT INTO `penggunaan` (`id`, `id_driver`, `id_taksi`, `penumpang`, `tanggal`, `layanan`, `tarif`) VALUES
-(3, 1, 1, 'as', '21', '10', 12);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `taksi`
---
-
-CREATE TABLE `taksi` (
-  `id` int(11) NOT NULL,
-  `merek` varchar(255) NOT NULL,
-  `jenis` varchar(255) NOT NULL,
-  `plat` varchar(15) NOT NULL,
-  `kapasitas` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `taksi`
---
-
-INSERT INTO `taksi` (`id`, `merek`, `jenis`, `plat`, `kapasitas`) VALUES
-(1, 'as', 'ads', '312', 2);
+(1, 'Exam', 12, 'Perempuan', '123', 'Jl. Melati'),
+(2, 'West', 33, 'Laki-Laki', '12321', 'Jl. Merdeka');
 
 --
 -- Indexes for dumped tables
@@ -120,53 +53,6 @@ INSERT INTO `taksi` (`id`, `merek`, `jenis`, `plat`, `kapasitas`) VALUES
 --
 ALTER TABLE `driver`
   ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `login`
---
-ALTER TABLE `login`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `penggunaan`
---
-ALTER TABLE `penggunaan`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `id_driver` (`id_driver`),
-  ADD KEY `id_taksi` (`id_taksi`);
-
---
--- Indexes for table `taksi`
---
-ALTER TABLE `taksi`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `login`
---
-ALTER TABLE `login`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-
---
--- AUTO_INCREMENT for table `penggunaan`
---
-ALTER TABLE `penggunaan`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `penggunaan`
---
-ALTER TABLE `penggunaan`
-  ADD CONSTRAINT `penggunaan_ibfk_1` FOREIGN KEY (`id_driver`) REFERENCES `driver` (`id`),
-  ADD CONSTRAINT `penggunaan_ibfk_2` FOREIGN KEY (`id_taksi`) REFERENCES `taksi` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
